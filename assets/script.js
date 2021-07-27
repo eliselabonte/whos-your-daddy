4891728785cfeba1eb223fb87f51c35595e2d298
Jef Mitchell <jef@project87.net>
2021-07-27 13:30:09 -0400

    Update: Javascript - Adjusted Giphy API url for better GIF results.

--- assets/script.js
+++ assets/script.js
@@ -3,11 +3,10 @@ const joke$ = $("#joke");
 
 // API pull for G rated dad joke GIFs.
 $.ajax({
-  url: "https://api.giphy.com/v1/gifs/random?api_key=IaSI9WvRX5rWEpsJJOZEduVTQGDHxWk6&tag=laughing&rating=g",
+  url: "https://api.giphy.com/v1/gifs/random?api_key=IaSI9WvRX5rWEpsJJOZEduVTQGDHxWk6&tag=chuckle&rating=g",
   type: "GET",
   success: function (results) {
     // $.data.images.original.url is the direct path to the GIF.
-    console.log(results.data.images.original.url);
     gif$.attr("src", results.data.images.original.url);
   },
   error: function (error) {
@@ -21,7 +20,6 @@ $.ajax({
   type: "GET",
   success: function (results) {
     // $.attachments[0].text is the direct path to the joke.
-    console.log(results.attachments[0].text);
     joke$.text(results.attachments[0].text);
   },
   error: function (error) {
