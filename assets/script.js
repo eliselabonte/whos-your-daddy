6dd9bbee2b2b188742e8ebaf40c51a2103e3a011
Jef Mitchell <jef@project87.net>
2021-07-31 11:11:40 -0400

    Javascript Update - Nested all ajax pulls under a single function. - Created generate joke button logic.

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,8 @@
+const welcomeCard$ = $("#welcomeCard");
+const jokeSection$ = $("#gifAndJoke")
+const jokeButton$ = $("#jokeButton");
+const favSection$ = $("#favorites-section");
+
 const gif$ = $("#gifDisplay");
 const joke$ = $("#jokeDisplay");
 const favButton$ = $("#favButton");
@@ -7,32 +12,34 @@ const favList$ = $("#favoritesList");
 let jokeArray = JSON.parse(localStorage.getItem("jokes")) || [];
 let currentJoke = "";
 
-// API pull for G rated dad joke GIFs.
-$.ajax({
-  url: "https://api.giphy.com/v1/gifs/random?api_key=IaSI9WvRX5rWEpsJJOZEduVTQGDHxWk6&tag=chuckle&rating=g",
-  type: "GET",
-  success: function (results) {
-    // $.data.images.original.url is the direct path to the GIF.
-    gif$.attr("src", results.data.images.original.url);
-  },
-  error: function (error) {
-    console.log(error);
-  },
-});
+function gifAndJokes() {
+  // API pull for G rated dad joke GIFs.
+  $.ajax({
+    url: "https://api.giphy.com/v1/gifs/random?api_key=IaSI9WvRX5rWEpsJJOZEduVTQGDHxWk6&tag=chuckle&rating=g",
+    type: "GET",
+    success: function (results) {
+      // $.data.images.original.url is the direct path to the GIF.
+      gif$.attr("src", results.data.images.original.url);
+    },
+    error: function (error) {
+      console.log(error);
+    },
+  });
 
-// API pull for random dad joke.
-$.ajax({
-  url: "https://icanhazdadjoke.com/slack",
-  type: "GET",
-  success: function (results) {
-    // $.attachments[0].text is the direct path to the joke.
-    joke$.text(results.attachments[0].text);
-    currentJoke = results.attachments[0].text;
-  },
-  error: function (error) {
-    console.log(error);
-  },
-});
+  // API pull for random dad joke.
+  $.ajax({
+    url: "https://icanhazdadjoke.com/slack",
+    type: "GET",
+    success: function (results) {
+      // $.attachments[0].text is the direct path to the joke.
+      joke$.text(results.attachments[0].text);
+      currentJoke = results.attachments[0].text;
+    },
+    error: function (error) {
+      console.log(error);
+    },
+  });
+}
 
 // Dynamicly add the favorited joke to the bottom of your favorites list.
 function addToFavList() {
@@ -42,9 +49,18 @@ function addToFavList() {
   }
 }
 
+// Reloads the page, generates joke, toggles html sections disokay.
+jokeButton$.on("click", function (e) {
+  gifAndJokes();
+  welcomeCard$.hide()
+  jokeSection$.show()
+  favSection$.show()
+});
+
 // Button to store the current joke and add it to the bottom of the favorites list.
-favButton$.on("click",  function (e) {
+favButton$.on("click", function (e) {
   jokeArray.push(currentJoke);
   localStorage.setItem("jokes", JSON.stringify(jokeArray));
   addToFavList();
 });
+
